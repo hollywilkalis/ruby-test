2dff58bc0684867d5a3c1bea2c8a073c0519dd33
hollywilkalis <holly.wilkalis@gmail.com>
2022-04-17 11:27:29 -0700

    Adding three word pphrase count

--- solution.rb
+++ solution.rb
@@ -11,7 +11,15 @@ def counter
     # creating a new Hash which will store the resulting count
     frequency = Hash.new(0)
     
-    words.each { |word| frequency[word] +=1 }
+    # words.each { |word| frequency[word] +=1 }
+
+    # replaceing the single word count from above with counting three word phrases
+    while words.size >= 3
+        frequency["#{words[0]} #{words[1]} #{words[2]}"] += 1
+        words.shift(1)
+      end
+    
+    #outputting count results as a code check
     puts frequency
 end
 
