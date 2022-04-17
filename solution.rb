3faf9e6b5fd12babd28d06b49d3f7b62098075d1
hollywilkalis <holly.wilkalis@gmail.com>
2022-04-17 13:46:27 -0700

    Replaced hard coded input with ARGV, added multi-file error, and one more file for testing

--- solution.rb
+++ solution.rb
@@ -1,6 +1,15 @@
 def counter
-    # grab file - hard coded to start with
-    book = File.read("moby-dick.txt")
+    # replacing hard coded file input with input from command line
+    # I can only handle one file for now so I'm putting in a hacky console error until I can refine
+    incomingFile = ARGV[0]
+
+    if ARGV.length != 1
+      puts "Sorry, at this time I can only accept one file at a time"
+      exit;
+    end
+
+    # Reading the file as specified
+    book = File.read(incomingFile)  
 
     # strip out case and punctuation except for apostrophe
     text = book.downcase.gsub(/[^a-z0-9\s^']/i, '')  
@@ -26,7 +35,8 @@ def counter
       end
     }
 
-    # I am spitting out entire list here and also reversing the sort - still deal with limiting the results
+    # I am spitting out entire list here and also reversing the sort
+    # still need to refine to limit the results to top 100
     sortedList = frequencyList.sort{|a,b| b[1]<=>a[1]}.each { |elem|
     puts "#{elem[1]} occurrences of \"#{elem[0]}\""}
 end
